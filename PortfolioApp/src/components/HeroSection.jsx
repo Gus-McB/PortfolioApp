462a772c96bb385ce4f164153c1e3f83c7947d51
Mcbac001 <mcbac001@mymail.unisa.edu.au>
2025-06-19 00:25:16 +0930

    Finished the NavBar, started the intro section

---
+++ PortfolioApp/src/components/HeroSection.jsx
@@ -0,0 +1,18 @@
+
+
+export const HeroSection = () => {
+    return <section 
+    id="hero" 
+    className="relative min-h-screen flex flex-col items-center justofy-center px-4" 
+    >
+        <div className="container max-w-4xl mx-auto text-center z-10">
+            <div className="space-y-6">
+                <h1>
+                    <span> I Am</span>
+                    <span> Angus</span>
+                    <span> McBride</span>
+                </h1>
+            </div>
+        </div>
+    </section>
+}
--- PortfolioApp/src/pages/Home.jsx
+++ PortfolioApp/src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import { ThemeToggle } from "../components/ThemeToggle";
 import { StarBackground } from "../components/StarBackground";
+import { NavBar } from "../components/NavBar";
+import { HeroSection } from "../components/HeroSection";
 
 export const Home = () => {
   return (
@@ -9,9 +11,11 @@ export const Home = () => {
       {/* Background Effect */}
       <StarBackground />
       {/* Navbar */}
-
+      <NavBar />
       {/* Main Content */}
-
+      <main>
+        <HeroSection />
+      </main>
       {/* Footer */}
 
     </div>
